eebcd7cb82aca3029043c2c83a13b1ac73d2298e
lu <a0960753785@gmail.com>
2025-08-08 00:03:17 +0800

    聯絡我們RWD

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log('Hello world');
+
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1.1,
+      spaceBetween: 24,
+      breakpoints: {
+    
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 4,
+    }    
+  }
+});
